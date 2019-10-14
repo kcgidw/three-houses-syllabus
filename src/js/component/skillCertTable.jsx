d7e3afdc7c7ff4989c5c0bbc5e94103543ebb6cc
Gideon Wong <kc.gideonw@gmail.com>
2019-10-13 23:45:03 -0700

    functional skillcerttable

--- src/js/component/skillCertTable.jsx
+++ src/js/component/skillCertTable.jsx
@@ -1,34 +1,33 @@
 import React from 'react';
 import SkillIcon from './skillIcon';
 
-export default class SkillCertTable extends React.Component {
-	constructor(props) {
-		super(props);
-		this.renderHeaderCells = this.renderHeaderCells.bind(this);
-		this.renderRowCells = this.renderRowCells.bind(this);
-	}
-	renderHeaderCells() {
-		return this.props.classData.relatedSkills.map((s) => {
-			return (<th key={s}><SkillIcon skill={s}></SkillIcon></th>);
-		});
-	}
-	renderRowCells(field) {
-		return this.props.classData.relatedSkills.map((skill) => {
-			return (<td key={field+':'+skill} className="bold">{this.props.classData[field][skill] || '-'}</td>);
-		});
-	}
-	render() {
-		if(this.props.classData.relatedSkills.length === 0) {
-			return <div className="">N/A</div>;
-		}
-		return (<table className="mini-table skill-cert-table">
-			<thead>
-				<tr>{this.renderHeaderCells()}</tr>
-			</thead>
-			<tbody>
-				<tr className={""}>{this.renderRowCells('certification')}</tr>
-				<tr className={""}>{this.renderRowCells('skillBonus')}</tr>
-			</tbody>
-		</table>);
-	}
+const renderHeaderCells = (classData) => {
+	return classData.relatedSkills.map((s) => {
+		return (<th key={s}><SkillIcon skill={s}></SkillIcon></th>);
+	});
 }
+const renderRowCells = (classData, field) => {
+	return classData.relatedSkills.map((skill) => {
+		return (<td key={field+':'+skill} className="bold">{classData[field][skill] || '-'}</td>);
+	});
+}
+
+const SkillCertTable = (props) => {
+	const {classData} = props;
+
+	if(classData.relatedSkills.length === 0) {
+		return <div className="">N/A</div>;
+	}
+
+	return (<table className="mini-table skill-cert-table">
+		<thead>
+			<tr>{renderHeaderCells(classData)}</tr>
+		</thead>
+		<tbody>
+			<tr className={""}>{renderRowCells(classData, 'certification')}</tr>
+			<tr className={""}>{renderRowCells(classData, 'skillBonus')}</tr>
+		</tbody>
+	</table>);
+}
+
+export default SkillCertTable;
